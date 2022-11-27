698912b3f9a880c63b71ce07a3d9c294da58c7a6
rnjsdbswo123 <64054467+rnjsdbswo123@users.noreply.github.com>
2022-11-27 13:24:30 +0900

    midlleweare

--- src/server.js
+++ src/server.js
@@ -3,17 +3,15 @@ const PORT = 4000;
 const app = express();
 
 
+
+
 const handleHome = (req, res) =>{
-    return res.end()
-
+    return res.send("<h1>i love middlewares</h1>")
 } 
-const handleLogin = (req, res) =>{
-    return res.send("<h1>Login here</h1>")
-}
-
-app.get("/",handleHome)
-app.get("/login", handleLogin)
 
 
-const handleListening = (req, res) => console.log(`Server listening on prot http://localhost:${PORT}`);
+app.get("/", handleHome);
+
+
+const handleListening = (req, res) => console.log(`Server listening on prot http://localhost:${PORT}😍`);
 app.listen(PORT, handleListening)
